2bb6a2463a0c63d1cf11a7b65c32b49b0a31daa6
gyakkun <xiaowufeixia@gmail.com>
2018-04-22 01:43:43 +0800

    4/5 Critter Alter: BlusterRunner finished.

--- projects/crittersForExercise/BlusterCritter.java
+++ projects/crittersForExercise/BlusterCritter.java
@@ -2,17 +2,20 @@ import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
 import info.gridworld.grid.Location;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class BlusterCritter extends Critter {
 
     private int lessThan;
 
-    public BlusterCritter(int count) {
+    public BlusterCritter(int courage) {
         super();
-        lessThan = count;
+        lessThan = courage;
     }
 
-    public int ActorsWithinTwoSteps() {
-        int howMany = 0;
+    public ArrayList<Actor> ActorsWithinTwoSteps() {
+        ArrayList<Actor> result = new ArrayList<Actor>();
         int myRow = getLocation().getRow();
         int myCol = getLocation().getCol();
         for (int rCtr = -2; rCtr < 2; rCtr++) {
@@ -22,15 +25,28 @@ public class BlusterCritter extends Critter {
                 if (getGrid().isValid(tmpLoc)) {
                     Actor tmpActor = getGrid().get(tmpLoc);
                     if (tmpActor instanceof Critter) {
-                        howMany++;
+                        result.add(tmpActor);
                     }
                 }
             }
         }
-        return howMany;
+        return result;
     }
 
+    public void colorChange(int val){
+        double CHANGING_FACTOR = 0.05;
+        Color c = getColor();
+        int red = (int) (c.getRed() * (1 + val * CHANGING_FACTOR));
+        int green = (int) (c.getGreen() * (1 + val * CHANGING_FACTOR));
+        int blue = (int) (c.getBlue() * (1 + val * CHANGING_FACTOR));
+        setColor(new Color(red, green, blue));
+    }
 
+    public void act(){
+        super.act();
+        int changingFactor = ActorsWithinTwoSteps().size() > 0 ? 1 : -1;
+        colorChange(changingFactor);
+    }
 
 
 }
