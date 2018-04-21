f91c2812f85403cf30c5c11f3aad34c40aa16693
gyakkun <xiaowufeixia@gmail.com>
2018-04-21 21:08:25 +0800

    0.5/5 Critters alter.
    ChameleonCritterAlter class finished.

--- projects/crittersForExercise/ChameleonCritterAlter.java
+++ projects/crittersForExercise/ChameleonCritterAlter.java
@@ -14,12 +14,14 @@
  * @author Chris Nevison
  * @author Barbara Cloud Wells
  * @author Cay Horstmann
+ * @author gyakkun, 2018.04.21
  */
 
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
 import info.gridworld.grid.Location;
 
+import java.awt.Color;
 import java.util.ArrayList;
 
 /**
@@ -28,14 +30,23 @@ import java.util.ArrayList;
  * The implementation of this class is testable on the AP CS A and AB exams.
  */
 public class ChameleonCritterAlter extends Critter {
+	
+	private static final double DARKENING_FACTOR = 0.05;
+	
     /**
      * Randomly selects a neighbor and changes this critter's color to be the
      * same as that neighbor's. If there are no neighbors, no action is taken.
      */
     public void processActors(ArrayList<Actor> actors) {
         int n = actors.size();
-        if (n == 0)
-            return;
+        if (n == 0) {
+            Color c = getColor();
+            int red = (int) (c.getRed() * (1 - DARKENING_FACTOR));
+            int green = (int) (c.getGreen() * (1 - DARKENING_FACTOR));
+            int blue = (int) (c.getBlue() * (1 - DARKENING_FACTOR));
+        	setColor(new Color(red, green, blue));
+        	return;
+        }
         int r = (int) (Math.random() * n);
 
         Actor other = actors.get(r);
