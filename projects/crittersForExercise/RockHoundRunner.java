32751c9153c82e8a04af724e971612c4b6e72cb3
gyakkun <xiaowufeixia@gmail.com>
2018-04-22 01:46:47 +0800

    (s) Code format change.

--- projects/crittersForExercise/RockHoundRunner.java
+++ projects/crittersForExercise/RockHoundRunner.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * AP(r) Computer Science GridWorld Case Study:
  * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
  *
@@ -10,7 +10,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * @author Chris Nevison
  * @author Barbara Cloud Wells
  * @author Cay Horstmann
@@ -28,10 +28,8 @@ import java.awt.*;
  * This class runs a world that contains critters. <br />
  * This class is not tested on the AP CS A and AB exams.
  */
-public class RockHoundRunner
-{
-    public static void main(String[] args)
-    {
+public class RockHoundRunner {
+    public static void main(String[] args) {
         ActorWorld world = new ActorWorld();
         world.add(new Location(7, 8), new Rock());
         world.add(new Location(3, 3), new Rock());
