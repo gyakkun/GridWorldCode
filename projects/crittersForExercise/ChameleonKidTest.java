c3ce351e7f5ddf5817d63fb12ada75f41bfb4516
gyakkun <xiaowufeixia@gmail.com>
2018-04-22 00:07:06 +0800

    1.5/5 Critter Alter: Create RockHound class.
    Create ChameleonKidTest with JUnit template by Eclipse.

---
+++ projects/crittersForExercise/ChameleonKidTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ChameleonKidTest {
+
+	@Test
+	void testChameleonKid() {
+		fail("尚未实现");
+	}
+
+	@Test
+	void testGetActors() {
+		fail("尚未实现");
+	}
+
+	@Test
+	void testGetLocationsInDirections() {
+		fail("尚未实现");
+	}
+
+}
