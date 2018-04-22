551f5530693905f18b3890d4a06588777757099d
gyakkun <xiaowufeixia@gmail.com>
2018-04-22 09:59:06 +0800

    5/6 Critter Alter: KingCrab and its runner finished.
    Using ref answer implement instead. No null pointer exp.

--- projects/critters/CrabCritter.java
+++ projects/critters/CrabCritter.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * AP(r) Computer Science GridWorld Case Study:
  * Copyright(c) 2005-2006 Cay S. Horstmann (http://horstmann.com)
  *
@@ -10,7 +10,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * @author Chris Nevison
  * @author Barbara Cloud Wells
  * @author Cay Horstmann
@@ -29,25 +29,22 @@ import java.util.ArrayList;
  * <br />
  * This class is not tested on the AP CS A and AB exams.
  */
-public class CrabCritter extends Critter
-{
-    public CrabCritter()
-    {
+public class CrabCritter extends Critter {
+    public CrabCritter() {
         setColor(Color.RED);
     }
 
     /**
      * A crab gets the actors in the three locations immediately in front, to its
      * front-right and to its front-left
+     *
      * @return a list of actors occupying these locations
      */
-    public ArrayList<Actor> getActors()
-    {
+    public ArrayList<Actor> getActors() {
         ArrayList<Actor> actors = new ArrayList<Actor>();
         int[] dirs =
-            { Location.AHEAD, Location.HALF_LEFT, Location.HALF_RIGHT };
-        for (Location loc : getLocationsInDirections(dirs))
-        {
+                {Location.AHEAD, Location.HALF_LEFT, Location.HALF_RIGHT};
+        for (Location loc : getLocationsInDirections(dirs)) {
             Actor a = getGrid().get(loc);
             if (a != null)
                 actors.add(a);
@@ -59,11 +56,10 @@ public class CrabCritter extends Critter
     /**
      * @return list of empty locations immediately to the right and to the left
      */
-    public ArrayList<Location> getMoveLocations()
-    {
+    public ArrayList<Location> getMoveLocations() {
         ArrayList<Location> locs = new ArrayList<Location>();
         int[] dirs =
-            { Location.LEFT, Location.RIGHT };
+                {Location.LEFT, Location.RIGHT};
         for (Location loc : getLocationsInDirections(dirs))
             if (getGrid().get(loc) == null)
                 locs.add(loc);
@@ -74,10 +70,8 @@ public class CrabCritter extends Critter
     /**
      * If the crab critter doesn't move, it randomly turns left or right.
      */
-    public void makeMove(Location loc)
-    {
-        if (loc.equals(getLocation()))
-        {
+    public void makeMove(Location loc) {
+        if (loc.equals(getLocation())) {
             double r = Math.random();
             int angle;
             if (r < 0.5)
@@ -85,31 +79,29 @@ public class CrabCritter extends Critter
             else
                 angle = Location.RIGHT;
             setDirection(getDirection() + angle);
-        }
-        else
+        } else
             super.makeMove(loc);
     }
-    
+
     /**
      * Finds the valid adjacent locations of this critter in different
      * directions.
+     *
      * @param directions - an array of directions (which are relative to the
-     * current direction)
+     *                   current direction)
      * @return a set of valid locations that are neighbors of the current
      * location in the given directions
      */
-    public ArrayList<Location> getLocationsInDirections(int[] directions)
-    {
+    public ArrayList<Location> getLocationsInDirections(int[] directions) {
         ArrayList<Location> locs = new ArrayList<Location>();
         Grid gr = getGrid();
         Location loc = getLocation();
-    
-        for (int d : directions)
-        {
+
+        for (int d : directions) {
             Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
             if (gr.isValid(neighborLoc))
                 locs.add(neighborLoc);
         }
         return locs;
-    }    
+    }
 }
--- projects/crittersForExercise/KingCrab.java
+++ projects/crittersForExercise/KingCrab.java
@@ -3,13 +3,50 @@ import info.gridworld.actor.Critter;
 import info.gridworld.actor.Rock;
 import info.gridworld.grid.Location;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class KingCrab extends CrabCritter {
 
+    public KingCrab() {
+        super();
+        setColor(Color.cyan);
+    }
+
+    /**
+     * Computes the rounded integer distance between two given locations.
+     */
+    public int distanceFrom(Location loc1, Location loc2) {
+        int x1 = loc1.getRow();
+        int y1 = loc1.getCol();
+        int x2 = loc2.getRow();
+        int y2 = loc2.getCol();
+        double dist = Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) + .5;
+        return (int) Math.floor(dist);
+    }
+
     public void processActors(ArrayList<Actor> actors) {
         for (Actor a : actors) {
+            boolean flag = false;
+            ArrayList<Location> locs =
+                    getGrid().getEmptyAdjacentLocations(a.getLocation());
+            for (Location loc : locs) {
+                if (distanceFrom(getLocation(), loc) > 1) {
+                    a.moveTo(loc);
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag)
+                a.removeSelfFromGrid();
+        }
+    }
+
+
+/*    public void processActors(ArrayList<Actor> actors) {
+        for (Actor a : actors) {
             Location tmpLoc = a.getLocation();
+            ArrayList<Location> emtAdjLocs = getGrid().getEmptyAdjacentLocations(tmpLoc);
             int dirTowardKing = tmpLoc.
                     getDirectionToward(getLocation());
             if (getGrid().isValid(
@@ -20,5 +57,5 @@ public class KingCrab extends CrabCritter {
                 a.removeSelfFromGrid();
             }
         }
-    }
+    }*/
 }
