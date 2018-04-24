3fa278ef9269ebae753caeced4c631591e8f4b96
gyakkun <xiaowufeixia@gmail.com>
2018-04-24 17:00:18 +0800

    Finally work with JUnit in IDEA.
    Modified the test source folder in module setting.
    
    See test/JumperTest.java, it's runnable.

---
+++ test/JumperTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JumperTest {
+
+    @Test
+    public void act() {
+        assertEquals("a","b");
+    }
+
+    @Test
+    public void jump() {
+        fail("just test :D");
+    }
+
+    @Test
+    public void canJump() {
+        fail("just test :D");
+    }
+}
